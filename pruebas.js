512f8c16082c304fc16fd66dd8a6bec6a4694711
Jonathan Hernández <jona73eli@gmail.com>
2026-04-01 22:44:43 -0600

    Lógica inicial JS

---
+++ pruebas.js
@@ -0,0 +1,126 @@
+let modulo = (()=>{
+    const btnAtacar = document.querySelector("#atacar"),
+        btnDefender = document.querySelector("#defender"),
+        btnCurarse = document.querySelector("#curarse"),
+        VidaJugador = document.querySelector("#jugador"),
+        vidaComputadora = document.querySelector("#computadora"),
+        suceso = document.querySelector("#movimiento");
+
+    let jugadorHP = 100;
+    let computadoraHP = 100;
+    let turnoJugador = true;
+    let defendiendo = false;
+
+    const atacar = ()=> {
+        computadoraHP -= Math.floor(Math.random() * (20 - 5 + 1)) + 5;
+        suceso.innerText = `El jugador realizo un ataque`;
+        turnoJugador = false;
+    }
+
+    const evaluarDaño = (daño)=> {
+        if(defendiendo){
+            daño = Math.floor(daño/2);
+            suceso.innerText = `La computadora realizo un ataque de ${daño}`
+            defendiendo = false;
+        }
+        jugadorHP -= daño;
+        suceso.innerText = `La computadora realizo un ataque de ${daño}`
+        turnoJugador = true;
+    }
+
+
+    const atacarComputadora = ()=> {
+        if(turnoJugador === false){
+            let daño = Math.floor(Math.random() * (20 - 5 + 1)) +1;
+            evaluarDaño(daño);
+        }
+    }
+
+    const atacarComputadoraMas = ()=> {
+        if(turnoJugador === false){
+            let daño = Math.floor(Math.random() * (40 - 20 + 1)) +1;
+            evaluarDaño(daño);
+        }
+    }
+
+    const defender = ()=> {
+        defendiendo = true;
+        suceso.innerText = `El jugador esta defendiendo`;
+        turnoJugador = false;
+    }
+
+    const curarse = ()=> {
+        jugadorHP += 15;
+        suceso.innerText = `El jugador se curo`;
+        turnoJugador = false;
+    }
+
+    const evaluar = ()=> {
+        if(jugadorHP > 100) jugadorHP = 100;
+        if(jugadorHP < 0) jugadorHP = 0;
+        if(computadoraHP < 0) computadoraHP = 0;
+        ganador();
+    }
+
+    const ganador = ()=> {
+        if(jugadorHP === 0) {
+            alert("Perdiste");
+            btnAtacar.disabled = true;
+            btnCurarse.disabled = true;
+            btnDefender.disabled = true;
+        } else if(computadoraHP === 0) {
+            alert("Ganaste")
+            btnAtacar.disabled = true;
+            btnCurarse.disabled = true;
+            btnDefender.disabled = true;
+        }
+    }
+
+    const renderizar = ()=> {
+        VidaJugador.innerText = `${jugadorHP}`;
+        vidaComputadora.innerText = `${computadoraHP}`;
+    }
+
+    const turnoEnemigo = ()=> {
+        let caso = Math.floor(Math.random() * 2);
+        switch(caso){
+            case 0:
+                atacarComputadora();
+                break;
+            case 1:
+                atacarComputadoraMas();
+                break;
+        }
+    }
+
+    const bloquearBotones = () => {
+        btnAtacar.disabled = true;
+        btnCurarse.disabled = true;
+        btnDefender.disabled = true;
+    }
+
+    const desbloquearBotones = () => {
+        btnAtacar.disabled = false;
+        btnCurarse.disabled = false;
+        btnDefender.disabled = false;
+    }
+
+    const turnoDeJugador = (accion)=>{
+        bloquearBotones();
+        accion();
+        evaluar();
+        renderizar();
+        setTimeout(()=>{
+            turnoEnemigo();
+            evaluar();
+            renderizar();
+            desbloquearBotones();
+        },1000);
+    }
+
+    btnAtacar.addEventListener("click", ()=> turnoDeJugador(atacar));
+
+    btnCurarse.addEventListener("click", ()=> turnoDeJugador(curarse));
+
+    btnDefender.addEventListener("click", ()=> turnoDeJugador(defender));
+})();
